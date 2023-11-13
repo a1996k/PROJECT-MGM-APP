926806c39fd9784d4f62a61d22759e91699346cc
a1996k <krishna.ab@northeastern.edu>
2023-11-13 13:04:57 -0500

    Updated with add and delete client

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,3 @@
-const {projects, clients} = require('../sampledata.js')
-
-
 // Mongoose Models
 
 const Client = require('../models/Client.js');
@@ -13,6 +10,7 @@ const {
      GraphQLString, 
      GraphQLSchema,
      GraphQLUnionType,
+     GraphQLNonNull
  } = require('graphql')
 
  //Project Type
@@ -77,6 +75,39 @@ const RootQuery = new GraphQLObjectType({
     }
 });
 
+// Mutations
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addClient: {
+            type: ClientType,
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) },
+                email: { type: GraphQLNonNull(GraphQLString) },
+                phone: { type: GraphQLNonNull(GraphQLString) },
+            },
+            resolve(parent, args) {
+                const client = new Client({
+                    name : args.name,
+                    email : args.email,
+                    phone : args.phone,
+                });
+                return client.save();
+            }
+        },
+        deleteClient: {
+            type: ClientType,
+            args: {
+                id: { type: GraphQLNonNull(GraphQLID) },
+            },
+            resolve(parent, args){
+                return Client.findByIdAndDelete(args.id);
+            },
+        },
+    },
+});
+
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation
 })
